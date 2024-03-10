7714a7f21de33627b490b688a2c61d3bcb9fc502
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-03-10 19:43:33 -0400

    Finished

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,66 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private final Character[][] gameBoard;
     public Board(Character[][] matrix) {
+        this.gameBoard = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        if(getWinner()=="X"){
+            return true;
+        }
+        else
+            return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        if(getWinner()=="O"){
+            return true;
+        }
+        else
+            return false;
+    }
+    private boolean checkSymbol(char symbol) {
+        // Checking for Rows
+        for (int i = 0; i < 3; i++) {
+            if (this.gameBoard[i][0] == symbol && this.gameBoard[i][1] == symbol && this.gameBoard[i][2] == symbol) {
+                return true;
+            }
+        }
+        // Checking for all Columns
+        for(int i=0;i<3;i++){
+            if(this.gameBoard[0][i] ==symbol && this.gameBoard[1][i] == symbol && this.gameBoard[2][i]== symbol){
+                return true;
+            }
+        }
+        // Checking diagonally
+        if(this.gameBoard[0][0] == symbol && this.gameBoard[1][1]==symbol && this.gameBoard[2][2]==symbol){
+            return true;
+        } else if (this.gameBoard[0][2] == symbol && this.gameBoard[1][1]==symbol && this.gameBoard[2][0]==symbol) {
+            return true;
+
+        }
+        else
+            return false;
+
     }
 
     public Boolean isTie() {
-        return null;
+        if(getWinner()=="")
+            return true;
+        else
+            return false;
     }
 
     public String getWinner() {
-        return null;
+        if(checkSymbol('X')){
+            return "X";
+        } else if (checkSymbol('O')) {
+            return "O";
+        }
+        else
+            return "";
     }
 
 }
